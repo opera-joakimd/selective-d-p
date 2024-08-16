5eda977ed8f6e9471ea086025ceff3015cc97821
opera-joakimd <joakimd@opera.com>
2024-08-16 11:05:24 +0200

    Change color of box shadow

--- colorChanger.js
+++ colorChanger.js
@@ -1,21 +1,24 @@
-const target = document.body;
-const config = { attributes: false, childList: true, subtree: true };
+const target = document.body
+const config = { attributes: false, childList: true, subtree: true }
 
-const observer = new MutationObserver(changeColor);
-observer.observe(target, config);
+const observer = new MutationObserver(changeColor)
+observer.observe(target, config)
 
 function changeColor() {
   const elementMatches = document.querySelectorAll(
     '[title="Status: Open"], [class*="color-bg-open-emphasis"]'
-  );
-  elementMatches.forEach((match) =>
-    match.setAttribute("style", "background-color:rgb(35, 134, 53) !important")
-  );
+  )
+  elementMatches.forEach((match) => {
+    match.setAttribute(
+      'style',
+      'background-color:rgb(35, 134, 53) !important; box-shadow:var(--boxShadow-thin) rgb(35, 134, 53) !important'
+    )
+  })
 
   const iconMatches = document.querySelectorAll(
     '[class*="color-fg-open"], [aria-label="Open pull request"], [class*="octicon-issue-opened"]'
-  );
+  )
   iconMatches.forEach((match) =>
-    match.setAttribute("style", "color:rgb(63, 185, 80) !important")
-  );
+    match.setAttribute('style', 'color:rgb(63, 185, 80) !important')
+  )
 }
